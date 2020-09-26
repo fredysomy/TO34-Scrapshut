df607451eb56c1974b54d2d13152e4a187c5e2b8
fredysomy <fredysomy@gmail.com>
2020-09-26 17:25:52 +0530

    added more models

--- ngo/admin.py
+++ ngo/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import User
+from .models import User,Products,ProductQuery
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Products)
+admin.site.register(ProductQuery)
--- ngo/models.py
+++ ngo/models.py
@@ -6,4 +6,23 @@ class User(models.Model):
     email=models.EmailField(max_length=50)
     admin=models.BooleanField()
     phno=models.IntegerField(max_length=10)
-    
+
+
+class Products(models.Model):
+    productname=models.CharField(max_length=100)
+    description=models.TextField(max_length=300)
+    quantity=models.IntegerField(max_length=100000)
+    donatorname=models.CharField(max_length=100)
+    donator_phno=models.IntegerField(max_length=12)
+    donatoremail=models.EmailField(max_length=400)
+    donator_address=models.TextField()
+
+class ProductQuery(models.Model):
+    productname=models.CharField(max_length=100)
+    request=models.TextField(max_length=300)
+    quantity_needed=models.IntegerField(max_length=100000)
+    reciever_name=models.CharField(max_length=100)
+    reciever_phno=models.IntegerField(max_length=12)
+    recieveremail=models.EmailField(max_length=400)
+    reciever_address=models.TextField()
+    needed_by=models.DateField()
