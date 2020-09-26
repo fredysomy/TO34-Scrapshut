dd232372dc83b31a6dc5811b6fa42d05070f1c1d
fredysomy <fredysomy@gmail.com>
2020-09-26 17:04:24 +0530

    made user models

--- ngo/models.py
+++ ngo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name=models.CharField(max_length=50)
+    username=models.CharField(max_length=50)
+    email=models.EmailField(max_length=50)
+    admin=models.BooleanField()
+    phno=models.IntegerField(max_length=10)
+    
